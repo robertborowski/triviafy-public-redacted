ef6d217bdff01accbba2f7240e95a063c5d32036
robert borowski <rborowski141@gmail.com>
2021-05-06 06:40:49 -0400

    testing slack OAuth

--- backend/all_pages/slack_confirm_page/slack_confirm_page_render.py
+++ backend/all_pages/slack_confirm_page/slack_confirm_page_render.py
@@ -15,7 +15,7 @@ def before_request():
     new_url = remove_www_from_domain_function(request.url)
     return redirect(new_url, code=301)
 
-@slack_confirm_page_render.route("/slack-confirm")
+@slack_confirm_page_render.route("/slack/confirm")
 def slack_confirm_page_render_function():
   """Returns: Renders the landing page"""  
   # Need to create a css unique key so that cache busting can be done
