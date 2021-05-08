620370ceda05524b4cabe56581a08dd1e4d185c5
robert borowski <rborowski141@gmail.com>
2021-05-08 04:51:58 -0400

    testing oauth

--- backend/all_pages/slack_confirm_page/slack_confirm_page_render.py
+++ backend/all_pages/slack_confirm_page/slack_confirm_page_render.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, redirect, request
+from flask import render_template, Blueprint, redirect, request, session
 from backend.utils.app_setup_before.check_if_url_www import check_if_url_www_function
 from backend.utils.app_setup_before.remove_www_from_domain import remove_www_from_domain_function
 from backend.utils.uuid_and_timestamp.create_uuid import create_uuid_function
