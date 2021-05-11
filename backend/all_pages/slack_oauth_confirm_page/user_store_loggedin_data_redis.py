ac6a0b6c9465152217a1bed6c3d2995ce0f5f850
robert borowski <rborowski141@gmail.com>
2021-05-11 07:16:51 -0400

    testing oauth login and redis cookies

--- backend/all_pages/slack_oauth_confirm_page/user_store_loggedin_data_redis.py
+++ backend/all_pages/slack_oauth_confirm_page/user_store_loggedin_data_redis.py
@@ -1,5 +1,5 @@
 from backend.db.connection.redis_connect_to_database import redis_connect_to_database_function
-import json
+#import json
 
 
 def user_store_loggedin_data_redis_function(user_nested_dict, get_cookie_value_from_browser):
@@ -8,26 +8,8 @@ def user_store_loggedin_data_redis_function(user_nested_dict, get_cookie_value_f
     # Connect to redis database pool (no need to close)
     redis_connection = redis_connect_to_database_function()
 
-    #===================
-    print('- - - - - - - - - - - - - - - - - - - -')
-    redis_connection.set('hello', 'goodbye')
-    print('- - - - -')
-    print(get_cookie_value_from_browser)
-    print(type(get_cookie_value_from_browser))
-    print('- - - - -')
-    print(user_nested_dict)
-    print(type(user_nested_dict))
-    print('- - - - -')
-    print('- - - - - - - - - - - - - - - - - - - -')
-    #===================
-
     # Upload dictionary to redis based on cookies
-    #user_nested_object_json = json.dumps(user_nested_dict)
-    #redis_connection.set(get_cookie_value_from_browser, user_nested_object_json)
-    #redis_connection.set('monkey', user_nested_object_json)
-    redis_connection.set('monkey', 'robot')
-    #redis_connection.set(get_cookie_value_from_browser, user_nested_dict)
-    redis_connection.set(get_cookie_value_from_browser, 'hello')
+    redis_connection.hmset(get_cookie_value_from_browser, user_nested_dict)
 
     return 'user info stored in redis database'
   except:
