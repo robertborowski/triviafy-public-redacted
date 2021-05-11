970c1058a123f568ff64a103129a1fc1231daddc
robert borowski <rborowski141@gmail.com>
2021-05-11 06:56:52 -0400

    testing oauth login and redis cookies

--- backend/all_pages/slack_oauth_confirm_page/user_store_loggedin_data_redis.py
+++ backend/all_pages/slack_oauth_confirm_page/user_store_loggedin_data_redis.py
@@ -13,17 +13,17 @@ def user_store_loggedin_data_redis_function(user_nested_dict, get_cookie_value_f
     redis_connection.set('hello', 'goodbye')
     print('- - - - -')
     print(get_cookie_value_from_browser)
-    type(get_cookie_value_from_browser)
+    print(type(get_cookie_value_from_browser))
     print('- - - - -')
     print(user_nested_dict)
-    type(user_nested_dict)
+    print(type(user_nested_dict))
     print('- - - - -')
     print('- - - - - - - - - - - - - - - - - - - -')
     #===================
 
     # Upload dictionary to redis based on cookies
     user_nested_object_json = json.dumps(user_nested_dict)
-    redis_connection.set(get_cookie_value_from_browser, user_nested_object_json.encode('utf-8'))
+    redis_connection.set(get_cookie_value_from_browser, user_nested_object_json)
     #unpacked_object = pickle.loads(redis_connection.get(get_cookie_value_from_browser))
     return 'user info stored in redis database'
   except:
