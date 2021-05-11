590852eafd19b12d23f88f2981389715f36cbdcb
robert borowski <rborowski141@gmail.com>
2021-05-11 07:20:06 -0400

    testing oauth login and redis cookies

--- backend/db/connection/redis_connect_to_database.py
+++ backend/db/connection/redis_connect_to_database.py
@@ -4,14 +4,14 @@ import os
 def redis_connect_to_database_function():
   """Connect to redis database"""
   try:
-    """
+
     # Connecting to Redis non-pool
     redis_connection = redis.Redis(
       host = os.environ.get('REDIS_HOST_NAME'),
       port = str(os.environ.get('REDIS_PORT')),
       password = os.environ.get('REDIS_PASSWORD'))
-    """
 
+    """
     # Connecting to Redis pool method
     pool = redis.ConnectionPool(
       host = os.environ.get('REDIS_HOST_NAME'),
@@ -19,6 +19,7 @@ def redis_connect_to_database_function():
       password = os.environ.get('REDIS_PASSWORD'),
       db=0)
     redis_connection = redis.Redis(connection_pool=pool)
+    """
   
   except:
     print('redis connection failed!')
