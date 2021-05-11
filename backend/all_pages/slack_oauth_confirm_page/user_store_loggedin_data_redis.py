6caa55c05ffd93b999f4bd585570c1ead55f1a65
robert borowski <rborowski141@gmail.com>
2021-05-11 06:30:37 -0400

    testing oauth login and redis cookies

--- backend/all_pages/slack_oauth_confirm_page/user_store_loggedin_data_redis.py
+++ backend/all_pages/slack_oauth_confirm_page/user_store_loggedin_data_redis.py
@@ -1,5 +1,6 @@
 from backend.db.connection.redis_connect_to_database import redis_connect_to_database_function
-import pickle
+import json
+
 
 def user_store_loggedin_data_redis_function(user_nested_dict, get_cookie_value_from_browser):
   """Store user login info obj in redis database based on browser cookies"""
@@ -8,8 +9,8 @@ def user_store_loggedin_data_redis_function(user_nested_dict, get_cookie_value_f
     redis_connection = redis_connect_to_database_function()
 
     # Upload dictionary to redis based on cookies
-    pickled_object = pickle.dumps(user_nested_dict)
-    redis_connection.set(get_cookie_value_from_browser, pickled_object)
+    user_nested_object_json = json.dumps(user_nested_dict)
+    redis_connection.set(get_cookie_value_from_browser, user_nested_object_json)
     #unpacked_object = pickle.loads(redis_connection.get(get_cookie_value_from_browser))
     return 'user info stored in redis database'
   except:
--- backend/utils/cached_login/cached_login_info.py
+++ backend/utils/cached_login/cached_login_info.py
@@ -1,12 +1,12 @@
 from backend.db.connection.redis_connect_to_database import redis_connect_to_database_function
-import pickle
+import json
 
 def cached_login_info_function(get_cookie_value_from_browser):
   """If user is cached in redis database then return their information so they do not have to manually log in again"""
   # Connect to redis database pool (no need to close)
   redis_connection = redis_connect_to_database_function()
   try:
-    unpacked_object = pickle.loads(redis_connection.get(get_cookie_value_from_browser))
+    unpacked_object = json.loads(redis_connection.get(get_cookie_value_from_browser))
   except:
     unpacked_object = 'No obj stored in redis for this browser cookie.'
   return unpacked_object
