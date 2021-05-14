493fff977c042ac72225ad07050324f297cde648
robert borowski <rborowski141@gmail.com>
2021-05-13 20:34:13 -0400

    navbar for user not signed in reusable template now works

--- static/javascript/page_blueprints_js/navbar_not_signed_in_js/nav.js
+++ static/javascript/page_blueprints_js/navbar_not_signed_in_js/nav.js
@@ -1,4 +1,4 @@
-class Nav extends HTMLElement {
+class NavbarClass extends HTMLElement {
   constructor() {
     super();
   }
@@ -19,7 +19,7 @@ class Nav extends HTMLElement {
           <ul>
             <li><a href="#">Login</a></li>
             <li><a href="#"><button class="btn btn-create-account">Add Triviafy</button></a></li>
-            <!--<a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands&client_id=2010284559270.2041074682000&state={{ slack_state_uuid_html }}"><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>-->
+            <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands&client_id=2010284559270.2041074682000&state={{ slack_state_uuid_html }}"><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
           </ul>
         </div>
       </nav>
@@ -27,4 +27,4 @@ class Nav extends HTMLElement {
   }
 }
 
-customElements.define('nav-component', Nav);
+customElements.define('nav-component', NavbarClass);
