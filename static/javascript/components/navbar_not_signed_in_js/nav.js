5a64a1fa0828ae7ab69d1371c42bb0480a5774e3
robert borowski <rborowski141@gmail.com>
2021-05-18 05:33:23 -0400

    quiz intro section done

--- static/javascript/components/navbar_not_signed_in_js/nav.js
+++ static/javascript/components/navbar_not_signed_in_js/nav.js
@@ -1,4 +1,4 @@
-class NavbarClass extends HTMLElement {  
+class NavbarNotSignedInClass extends HTMLElement {  
   constructor() {
     super();
 
@@ -22,7 +22,6 @@ class NavbarClass extends HTMLElement {
           <ul>
             <li><a href="#">Login</a></li>
             <li><a href="#"><button class="btn btn-create-account">Add Triviafy</button></a></li>
-            <!-- <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands&client_id=2010284559270.2041074682000&state={{ slack_state_uuid_html }}"><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a> -->
             <a href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook,commands&client_id=2010284559270.2041074682000&state=${this.getAttribute("slack_state_uuid_js")}"><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
           </ul>
         </div>
@@ -31,4 +30,4 @@ class NavbarClass extends HTMLElement {
   }
 }
 
-customElements.define('nav-not-signed-in-component', NavbarClass);
+customElements.define('nav-not-signed-in-component', NavbarNotSignedInClass);
