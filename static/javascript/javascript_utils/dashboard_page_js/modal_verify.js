6aaeeb9d632b163b07c5f96e0c4f3595afc8ce9c
robert borowski <rborowski141@gmail.com>
2021-06-08 08:06:35 -0400

    dashboard modal fixed

--- static/javascript/javascript_utils/dashboard_page_js/modal_verify.js
+++ static/javascript/javascript_utils/dashboard_page_js/modal_verify.js
@@ -5,6 +5,8 @@ var quizRulesActivateModal = document.getElementById('quiz-rules-activate-modal'
 // Get open modal button
 var closeModalXButton = document.getElementsByClassName('close-modal-x-button')[0];
 
+
+
 // Listen for open click
 quizRulesActivateModal.addEventListener('click', openModal);
 // Listen for open click
@@ -12,14 +14,18 @@ closeModalXButton.addEventListener('click', closeModal);
 // Listen for outside click
 window.addEventListener('click', outsideClick);
 
+
+
 // Function to open modal
 function openModal(){
   modalDetails.style.display = 'block';
 }
+
 // Function to close modal
 function closeModal(){
   modalDetails.style.display = 'none';
 }
+
 // Function to close modal if outside click
 function outsideClick(e){
   if(e.target == modalDetails){
