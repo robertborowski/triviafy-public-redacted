faa3dfe8872baf2acb0fa1a39c6a4abc30a5d998
robert borowski <rborowski141@gmail.com>
2021-06-16 07:49:12 -0400

    index typeform setup

--- static/javascript/components/navbar_not_signed_in_js/nav.js
+++ static/javascript/components/navbar_not_signed_in_js/nav.js
@@ -30,7 +30,10 @@ class NavbarNotSignedInClass extends HTMLElement {
           <!-- <li><a href="#">How It Works</a></li> -->
           <li><a href="${this.getAttribute("link_about_js")}">About</a></li>
           <!-- <li><a href="#">Add Triviafy</a></li> -->
-          <li><a href="#"><button class="button-default button-default-color-primary" type="submit" value="submit" onclick="return clickSubmitButtonOnce()">Add Triviafy</button></a></li>
+          <!-- <li><a href="#"><button class="button-default button-default-color-primary" type="submit" value="submit" onclick="return clickSubmitButtonOnce()">Add Triviafy</button></a></li> -->
+          
+          <li><div style="text-align:center;"> <a class="typeform-share button" href="https://form.typeform.com/to/NPVjDw88?typeform-medium=embed-snippet" data-mode="popup" style="display:inline-block;text-decoration:none;background-color:#00C096;color:white;cursor:pointer;font-family:Helvetica,Arial,sans-serif;font-size:20px;line-height:50px;text-align:center;margin:0;height:50px;padding:0px 33px;border-radius:12px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;" data-size="100" target="_blank">Add Triviafy </a> </div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script></li>
+
         </ul>
       </div>
     </nav>
