e89eff07649adc6e50f8d20fec71ea118191fb93
robert borowski <rborowski141@gmail.com>
2021-08-25 07:18:50 -0400

    sign in with slack backend testing

---
+++ backend/page_templates_backend/slack_sign_in_with_slack_page_backend/slack_oauth_checking_database_for_user.py
@@ -0,0 +1,62 @@
+# -------------------------------------------------------------- Imports
+from backend.db.connection.postgres_connect_to_database import postgres_connect_to_database_function
+from backend.db.connection.postgres_close_connection_to_database import postgres_close_connection_to_database_function
+from backend.utils.localhost_print_utils.localhost_print import localhost_print_function
+from backend.db.queries.select_queries.select_queries_triviafy_user_login_information_table_slack.select_if_slack_user_authed_id_exists import select_if_slack_user_authed_id_exists_function
+from backend.utils.slack.user_info_data_manipulation.transpose_slack_user_data_to_nested_dict import transpose_slack_user_data_to_nested_dict_function
+
+# -------------------------------------------------------------- Main Function
+def slack_oauth_checking_database_for_user_function(response_authed_user_id):
+  localhost_print_function('=========================================== slack_oauth_checking_database_for_user_function START ===========================================')
+
+  # Connect to Postgres database
+  postgres_connection, postgres_cursor = postgres_connect_to_database_function()
+
+  # Get user login info from db
+  user_db_object = select_if_slack_user_authed_id_exists_function(postgres_connection, postgres_cursor, response_authed_user_id)
+
+  # If user does not exist in db
+  if user_db_object == None:
+    authed_user_id_already_exists = False
+    authed_user_id_signed_in_object = None
+  # If user does exist in db
+  else:
+    # ------------------------ Account Already Exist START ------------------------
+    slack_db_uuid = user_db_object[0]
+    slack_db_timestamp_created = user_db_object[1]
+    slack_guess_first_name = user_db_object[2]
+    slack_guess_last_name = user_db_object[3]
+    slack_authed_user_real_full_name = user_db_object[4]
+    slack_authed_user_email = user_db_object[6]
+    slack_authed_user_id = user_db_object[7]
+    slack_authed_team_id = user_db_object[8]
+    slack_authed_team_name = user_db_object[9]
+    slack_authed_channel_id = user_db_object[10]
+    slack_authed_channel_name = user_db_object[11]
+    company_name = user_db_object[12]
+    slack_authed_bot_user_id = user_db_object[13]
+    first_user_payment_admin = user_db_object[14]
+    slack_authed_token_type = user_db_object[15]
+    slack_authed_access_token = user_db_object[16]
+    slack_authed_user_timezone = user_db_object[17]
+    slack_authed_user_timezone_label = user_db_object[18]
+    slack_authed_user_timezone_offset = user_db_object[19]
+    slack_authed_user_job_title = user_db_object[20]
+    user_slack_email_permission_granted = user_db_object[21]
+    slack_authed_webhook_url = user_db_object[22]
+    # ------------------------ Account Already Exist END ------------------------
+
+    # ------------------------ Transpose the SQL pulled table to dict START ------------------------
+    # Transpose user data to nested dictionary. Make timestamp a string because you cannot upload timestamp to redis as a json obj
+    user_nested_dict = transpose_slack_user_data_to_nested_dict_function(slack_db_uuid, str(slack_db_timestamp_created), slack_guess_first_name, slack_guess_last_name, slack_authed_user_real_full_name, slack_authed_user_email, slack_authed_user_id, slack_authed_team_id, slack_authed_team_name, slack_authed_channel_id, slack_authed_channel_name, company_name, slack_authed_bot_user_id, first_user_payment_admin,  slack_authed_token_type, slack_authed_access_token, slack_authed_user_timezone, slack_authed_user_timezone_label, slack_authed_user_timezone_offset, slack_authed_user_job_title, user_slack_email_permission_granted, slack_authed_webhook_url)
+    # ------------------------ Transpose the SQL pulled table to dict END ------------------------
+
+    authed_user_id_already_exists = True
+    authed_user_id_signed_in_object = user_nested_dict
+
+
+  # Close postgres db connection
+  postgres_close_connection_to_database_function(postgres_connection, postgres_cursor)
+
+  localhost_print_function('=========================================== slack_oauth_checking_database_for_user_function END ===========================================')
+  return authed_user_id_already_exists, authed_user_id_signed_in_object
---
+++ backend/page_templates_backend/slack_sign_in_with_slack_page_backend/slack_oauth_redirect_page_index.py
@@ -0,0 +1,101 @@
+# -------------------------------------------------------------- Imports
+from flask import Blueprint, redirect, request, session
+from backend.utils.page_www_to_non_www.check_if_url_www import check_if_url_www_function
+from backend.utils.page_www_to_non_www.remove_www_from_domain import remove_www_from_domain_function
+from backend.utils.uuid_and_timestamp.create_uuid import create_uuid_function
+import os
+from slack_sdk import WebClient
+from backend.db.connection.redis_connect_to_database import redis_connect_to_database_function
+from backend.page_templates_backend.slack_confirm_oauth_redirect_dashboard_backend.user_store_loggedin_data_redis import user_store_loggedin_data_redis_function
+from backend.utils.localhost_print_utils.localhost_print import localhost_print_function
+import time
+from backend.page_templates_backend.slack_sign_in_with_slack_page_backend.slack_oauth_checking_database_for_user import slack_oauth_checking_database_for_user_function
+
+# -------------------------------------------------------------- App Setup
+slack_oauth_redirect_page_index = Blueprint("slack_oauth_redirect_page_index", __name__, static_folder="static", template_folder="templates")
+@slack_oauth_redirect_page_index.before_request
+def before_request():
+  www_start = check_if_url_www_function(request.url)
+  if www_start:
+    new_url = remove_www_from_domain_function(request.url)
+    return redirect(new_url, code=302)
+
+# -------------------------------------------------------------- App
+@slack_oauth_redirect_page_index.route("/slack/oauth_redirect", methods=['GET'])
+def slack_oauth_redirect_page_index_function():
+  localhost_print_function('=========================================== /slack/oauth_redirect Page START ===========================================')
+  
+  # ------------------------ CSS support START ------------------------
+  # Need to create a css unique key so that cache busting can be done
+  cache_busting_output = create_uuid_function('css_')
+  # ------------------------ CSS support END ------------------------
+
+
+  # ------------------------ Check Server Running START ------------------------
+  # -------------------------------------------------------------- Running on localhost
+  server_env = os.environ.get('TESTING', 'false')
+  # If running on localhost
+  if server_env == 'true':
+    # Connect to redis database pool (no need to close)
+    redis_connection = redis_connect_to_database_function()
+
+    # Get key:value from redis
+    localhost_slack_state_key = 'localhost_slack_state_key'
+    slack_state_value_passed_in_url = redis_connection.get(localhost_slack_state_key).decode('utf-8')
+
+    # Get key:value from redis
+    localhost_redis_browser_cookie_key = 'localhost_redis_browser_cookie_key'
+    get_cookie_value_from_browser = redis_connection.get(localhost_redis_browser_cookie_key).decode('utf-8')
+
+  # -------------------------------------------------------------- NOT running on localhost
+  else:
+    slack_state_value_passed_in_url = session['slack_state_uuid_value']
+    get_cookie_value_from_browser = request.cookies.get('triviafy_browser_cookie')
+  # ------------------------ Check Server Running END ------------------------
+
+
+  # ------------------------ Supporting Variables START ------------------------
+  my_slack_client_id = os.environ.get('SLACK_CLIENT_ID')
+  my_slack_client_secret = os.environ.get('SLACK_CLIENT_SECRET')
+  # ------------------------ Supporting Variables END ------------------------
+
+
+  # ------------------------ Sign in with Slack START ------------------------
+  # Retrieve the auth code and state from the request params
+  if "code" in request.args:
+    state_received = request.args['state']
+    if state_received == slack_state_value_passed_in_url:
+      code_received = request.args["code"]
+      try:
+        # Set up client
+        slack_bot_token = os.environ.get('SLACK_BOT_TOKEN')
+        token_response = WebClient(token=slack_bot_token).oauth_v2_access(
+            client_id = my_slack_client_id,
+            client_secret = my_slack_client_secret,
+            code = code_received,
+            redirect_uri = "https://triviafy.com/slack/oauth_redirect"
+        )
+        response_authed_user_id = token_response['authed_user']['id']
+        # ------------------------ Check if user exists START ------------------------
+        authed_user_id_already_exists, authed_user_id_signed_in_object = slack_oauth_checking_database_for_user_function(response_authed_user_id)
+        # ------------------------ Check if user exists END ------------------------
+        if authed_user_id_already_exists == False or authed_user_id_signed_in_object == None:
+          localhost_print_function('=========================================== /slack/oauth_redirect Page END ===========================================')
+          return redirect('/logout', code=302)
+        else:
+          # Store in redis
+          user_store_in_redis_status = user_store_loggedin_data_redis_function(authed_user_id_signed_in_object, get_cookie_value_from_browser)
+          localhost_print_function(user_store_in_redis_status)
+          time.sleep(2)
+          localhost_print_function('=========================================== /slack/oauth_redirect Page END ===========================================')
+          return redirect("/dashboard", code=302)
+
+      except:
+        localhost_print_function('no token response received')
+        pass
+  # ------------------------ Sign in with Slack END ------------------------
+
+
+  localhost_print_function('=========================================== /slack/oauth_redirect Page END ===========================================')
+  # Render the login page template, pass in the redis nested dict of all user info
+  return redirect('/logout', code=302)
