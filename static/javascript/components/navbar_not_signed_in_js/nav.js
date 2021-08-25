806414e0f94abd1e019d54cbebf463ec57ded34c
robert borowski <rborowski141@gmail.com>
2021-08-25 07:42:19 -0400

    sign in with slack backend testing

--- static/javascript/components/navbar_not_signed_in_js/nav.js
+++ static/javascript/components/navbar_not_signed_in_js/nav.js
@@ -28,12 +28,10 @@ class NavbarNotSignedInClass extends HTMLElement {
       <div class="navbar-links-not-signed-in">
         <ul>
           <li><a href="${this.getAttribute("link_about_js")}">About</a></li>
-          <li><a href="https://slack.com/oauth/v2/authorize?client_id=2010284559270.2041074682000&scope=incoming-webhook,team:read,users.profile:read,users:read,users:read.email&state=${this.getAttribute("slack_state_uuid_js")}&user_scope=openid,profile,email"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" class="slack-image-button-li" /></a></li>
-
-
+          
           <li><a href="https://slack.com/openid/connect/authorize?response_type=code&scope=openid%20profile%20email&client_id=2010284559270.2041074682000&state=${this.getAttribute("slack_state_uuid_js")}&redirect_uri=https://triviafy.com/slack/oauth_redirect"><img alt="Sign in with Slack" height="40" width="170" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" class="slack-image-button-li" /></a></li>
 
-
+          <li><a href="https://slack.com/oauth/v2/authorize?client_id=2010284559270.2041074682000&scope=incoming-webhook,team:read,users.profile:read,users:read,users:read.email&state=${this.getAttribute("slack_state_uuid_js")}&user_scope=openid,profile,email"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" class="slack-image-button-li" /></a></li>
         </ul>
       </div>
     </nav>
