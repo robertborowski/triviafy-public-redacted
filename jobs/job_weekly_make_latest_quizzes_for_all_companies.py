5bacfced3a4f29bfcb689236eea13992539fbc0e
robert borowski <rborowski141@gmail.com>
2021-10-06 08:06:14 -0400

    added logic so that before making the weekly quiz it checks if the company team free trial is expired AND then checks if company paid their latest month if so then it creates a quiz

--- jobs/job_weekly_make_latest_quizzes_for_all_companies.py
+++ jobs/job_weekly_make_latest_quizzes_for_all_companies.py
@@ -8,6 +8,8 @@ from backend.utils.latest_quiz_utils.get_latest_company_quiz_if_exists import ge
 from backend.utils.latest_quiz_utils.make_company_latest_quiz import make_company_latest_quiz_function
 import os, time
 from backend.utils.localhost_print_utils.localhost_print import localhost_print_function
+from backend.db.queries.select_queries.select_queries_triviafy_free_trial_tracker_slack_table.select_triviafy_free_trial_tracker_slack_table_end_timestamp import select_triviafy_free_trial_tracker_slack_table_end_timestamp_function
+from backend.db.queries.select_queries.select_queries_triviafy_slack_payment_status_table.select_triviafy_slack_payment_status_table_team_channel_year_month_combo_status_only import select_triviafy_slack_payment_status_table_team_channel_year_month_combo_status_only_function
 
 # -------------------------------------------------------------- Main Function
 def job_weekly_make_latest_quizzes_for_all_companies_function():
@@ -76,6 +78,56 @@ def job_weekly_make_latest_quizzes_for_all_companies_function():
     # ------------------------ Check If Latest Quiz Already Exists END ------------------------
     # ------------------------ Make The Latest Quiz For The Week START ------------------------
     else:
+      # ------------------------ Check If Free Trial Expired START ------------------------
+      # Connect to Postgres database
+      postgres_connection, postgres_cursor = postgres_connect_to_database_function()
+      
+      # Select free trial end date for company team
+      company_team_free_trial_end_timestamp = select_triviafy_free_trial_tracker_slack_table_end_timestamp_function(postgres_connection, postgres_cursor, slack_workspace_team_id, slack_channel_id)
+
+      # Close postgres db connection
+      postgres_close_connection_to_database_function(postgres_connection, postgres_cursor)
+      
+      # Set variables
+      today_date_record = date.today()
+      free_trial_expired_for_company_team = False
+
+      # Assign variable
+      if today_date_record > company_team_free_trial_end_timestamp[0].date():
+        free_trial_expired_for_company_team = True
+      elif today_date_record <= company_team_free_trial_end_timestamp[0].date():
+        free_trial_expired_for_company_team = False
+      # ------------------------ Check If Free Trial Expired END ------------------------
+
+
+      # ------------------------ Check If Paid Latest Month START ------------------------
+      if free_trial_expired_for_company_team == True:
+        # ------------------------ Get Today's Date Information START ------------------------
+        # Today's date
+        today_date = date.today()
+        today_date_split_arr = str(today_date).split('-')
+        # Separate Today's date into year month and day
+        today_date_year = today_date_split_arr[0]
+        today_date_month = today_date_split_arr[1]
+        today_date_date = today_date_split_arr[2]
+        # ------------------------ Get Today's Date Information END ------------------------
+
+        # Connect to Postgres database
+        postgres_connection, postgres_cursor = postgres_connect_to_database_function()
+
+        # Select if company paid latest month
+        company_paid_latest_month = select_triviafy_slack_payment_status_table_team_channel_year_month_combo_status_only_function(postgres_connection, postgres_cursor, slack_workspace_team_id, slack_channel_id, today_date_year, today_date_month)
+
+        # Close postgres db connection
+        postgres_close_connection_to_database_function(postgres_connection, postgres_cursor)
+        
+        if company_paid_latest_month[0] == False:
+          localhost_print_function('Cannot make a quiz for this company team because their free trial ended and they did not pay the latest month')
+          continue
+      # ------------------------ Check If Paid Latest Month END ------------------------
+
+
+      # ------------------------ Check If Free Trial Expired / Latest Month Paid END ------------------------
       latest_company_quiz_object = make_company_latest_quiz_function(function_input_dict, indv_company_quiz_settings_arr)
       localhost_print_function('Made the latest company quiz from scratch and stored in DB')
     # ------------------------ Make The Latest Quiz For The Week END ------------------------
