577b2855d8a347de342339a00ba9a57cefdfb86f
robert borowski <rborowski141@gmail.com>
2021-11-06 07:45:45 -0400

    PDFs

--- static/javascript/components/footer_not_signed_in_js/footer_2.js
+++ static/javascript/components/footer_not_signed_in_js/footer_2.js
@@ -16,8 +16,8 @@ class FooterNotSignedInClass2 extends HTMLElement {
         <div class="footer-not-signed-in-2-sub-section">
           <p class="footer-title footer-title-position">Product</p>
           <ul class="footer-not-signed-in-2-ul">
-            <li class="footer-sub-title-item footer-sub-title-item-position"><a href="${this.getAttribute("pdf_example_quiz_js")}" class="footer-a">Example Quiz (PDF)</a></li>
-            <li class="footer-sub-title-item footer-sub-title-item-position"><a href="${this.getAttribute("pdf_slack_setup_js")}" class="footer-a">Slack Setup (PDF)</a></li>
+            <li class="footer-sub-title-item footer-sub-title-item-position"><a href="${this.getAttribute("pdf_example_quiz_js")}" class="footer-a">Example Quiz</a></li>
+            <li class="footer-sub-title-item footer-sub-title-item-position"><a href="${this.getAttribute("pdf_slack_setup_js")}" class="footer-a">Slack Setup</a></li>
           </ul>
         </div>
 
